dcc59812a4a12dc1c4419e53df7dee3f2114590f
Von Christian Halip <vc.halip@gmail.com>
2017-01-24 10:00:42 +0800

    Implement creating training feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :trainings, only: [:index, :new, :create]
   resources :trainees, except: [:destroy]
   resources :qualifications, except: [:destroy] do
     resources :competencies, only: [:new, :create], module: :qualifications
