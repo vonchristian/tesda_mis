182d3fd775f0114acb0bd87bd456c52e8af60fe4
Von Christian Halip <vc.halip@gmail.com>
2017-02-24 12:01:17 +0800

    Add motorcycle servicing nc ii seed file

---
+++ db/seeds/qualifications/motorcycle_small_engine_servicing_nc_ii.rb
@@ -0,0 +1,22 @@
+sector = Sector.find_or_create_by(name: "Automotive and Land Transport")
+qualification = Qualification.find_or_create_by!(name: "Motorcycle/Small Engine Servicing NC II", sector: sector)
+
+Qualifications::Competency.find_or_create_by(competency_type: 0, unit_code: '500311105', unit_title: "Participate in Workplace Communication", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 0, unit_code: '500311106', unit_title: "Work in a Team Environment", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 0, unit_code: '500311107', unit_title: "Practice Career Professionalism", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 0, unit_code: '500311108', unit_title: "Practice Occupational Health and Safety Procedures", qualification: qualification)
+
+Qualifications::Competency.find_or_create_by(competency_type: 1, unit_code: 'ALT723201', unit_title: "Apply Appropriate Sealant/Adhesive", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 1, unit_code: 'ALT723202', unit_title: "Move and Position Vehicle", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 1, unit_code: 'ALT311202', unit_title: "Perform Mensuration and Calculation", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 1, unit_code: 'ALT723203', unit_title: "Read, Interpret and Apply Specifications and Manuals", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 1, unit_code: 'ALT723204', unit_title: "Use and Apply Lubricants/Coolants", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 1, unit_code: 'ALT723205', unit_title: "Perform Shop Maintenance", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 1, unit_code: 'ALT311211', unit_title: "Prepare Job Estimate/Costing", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 1, unit_code: 'ALT311212', unit_title: "Observe Quality Systems", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 1, unit_code: 'ALT723213', unit_title: "Perform Periodic Maintenance", qualification: qualification)
+
+Qualifications::Competency.find_or_create_by(competency_type: 2, unit_code: 'ALT723372', unit_title: "Service Motorcycle/Small Engine System", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 2, unit_code: 'ALT723373', unit_title: "Service Electrical System", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 2, unit_code: 'ALT723374', unit_title: "Service Chassis", qualification: qualification)
+Qualifications::Competency.find_or_create_by(competency_type: 2, unit_code: 'ALT723375', unit_title: "Overhaul Motorcycle/Small Engine", qualification: qualification)
